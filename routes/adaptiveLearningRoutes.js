48dc6f02f47a0299107ef5709642341ea3ce23b8
Adeline A. Ackun <adelineackun@gmail.com>
2024-08-17 21:53:03 +0000

    Changes

--- routes/adaptiveLearningRoutes.js
+++ routes/adaptiveLearningRoutes.js
@@ -3,6 +3,6 @@ import { getAdaptiveContent } from '../controllers/adaptiveLearningController.js
 
 const adaptiveContentRouter = Router();
 
-adaptiveContentRouter.get('/adaptive-content/:userId', getAdaptiveContent);
+adaptiveContentRouter.get('/adaptive-content/:Id', getAdaptiveContent);
 
 export default adaptiveContentRouter;
